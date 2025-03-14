065f0ef83884d0a4b42d42b0e78b5c0df32b72aa
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-03-14 14:21:06 -0300

    criação do titulo com angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CadastrarComponent } from './menu/cadastrar/cadastrar.component';
+import { ConsultarComponent } from './menu/consultar/consultar.component';
+import { HomeComponent } from './menu/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },  // Página inicial
+    { path: 'sobre', component: CadastrarComponent },
+    { path: 'contato', component: ConsultarComponent }
+];
--- src/app/menu/home/home.component.ts
+++ src/app/menu/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
